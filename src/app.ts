9a277a17eed73653a8acb8ac4dd1213951e5d559
md-suhag <abdussalamsuhag104@gmail.com>
2025-12-25 14:34:09 +0600

    update: express data parser limit

--- src/app.ts
+++ src/app.ts
@@ -28,8 +28,8 @@ app.use(Morgan.errorHandler);
 
 // body parser
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 // file retrieve
 app.use(express.static("uploads"));
